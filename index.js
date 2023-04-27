7e13536c304ca4b179129e957c48dca653ad0be5
htschi <125224273+htschi@users.noreply.github.com>
2023-04-27 15:18:49 +0700

    Add Routes

--- index.js
+++ index.js
@@ -1,8 +1,87 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Welcome to Google Search')
+})
+
+let data = [
+  {
+    title: 'JS Tutorials',
+    description: 'The best Javascript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js',
+      },
+      {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js',
+      },
+    ],
+  },
+  {
+    title: 'JS Tutorials1',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners1',
+        url: 'https://www.w3schools.com/js',
+      },
+      {
+        title: 'JS for the Web1',
+        url: 'https://www.w3schools.com/js',
+      },
+    ],
+  },
+  {
+    title: 'JS Tutorials2',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners2',
+        url: 'https://www.w3schools.com/js',
+      },
+      {
+        title: 'JS for the Web2',
+        url: 'https://www.w3schools.com/js',
+      },
+    ],
+  },
+]
+
+app.get('/results', (req, res) => {
+  // let str = req.query.search
+  let filteredData = []
+
+  function filterData(str) {
+    let count = 0
+    data.map((e) => {
+      if (e.title.toUpperCase().includes(str.toUpperCase())) {
+        filteredData[count] = e
+        count++
+      } else {
+        if (e.description.toUpperCase().includes(str.toUpperCase())) {
+          filteredData[count] = e
+          count++
+        } else {
+          if (e.url.toUpperCase().includes(str.toUpperCase())) {
+            filteredData[count] = e
+            count++
+          }
+        }
+      }
+      return filteredData
+    })
+  }
+  filterData(req.query.search)
+  res.send(filteredData)
 })
 
 app.listen(4000, () => {
